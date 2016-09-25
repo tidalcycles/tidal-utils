79799d30821aa98031f2fa1f66a59b4d762a260f
alex <alex@slab.org>
2016-09-25 23:56:18 +0100

    cycle position vis

--- liveinfo.hs
+++ liveinfo.hs
@@ -1,6 +1,24 @@
 import Sound.Tidal.Tempo
 
-main = clocked onTick
+tpb = 12
+
+main = clockedTick tpb onTick
+
+vis steps n = "["
+              ++ (map (\x -> if (x == step) then 'X' else '-') [0 .. steps-1])
+              ++ "]"
+  where step = (n `mod` tpb) `div` (tpb `div` steps)
 
 onTick tempo tick =
-  putStr $ "cps: " ++ show (cps tempo) ++ " cycle: " ++ show tick ++ "   \r"
+  putStr ("cps: " ++ show (cps tempo) ++ " "
+          ++ (vis 4 tick)
+          ++ " "
+          ++ (vis 4 (tick `div` 4))
+          ++ " "
+          ++ (vis 3 tick)
+          ++ " "
+          ++ (vis 3 (tick `div` 3))
+          ++ " cycle: " ++ show (tick `div` tpb)
+          ++ "   \r"
+         )
+  
